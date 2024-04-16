0836848f69e0ecd3f1c89726f89979511421f736
Raphael <raphael.lukas.m@gmail.com>
2024-04-16 16:12:30 -0300

    feat: add first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { SignIn } from "./screens/signin";
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export function SignIn() {
   return (
     <>
-      <h1>Sign In</h1>
+      <Button>Sign In</Button>
     </>
   );
 }
