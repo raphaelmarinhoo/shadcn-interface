b053a0bab8b6b24e600d3b52d7b0c688c545c963
Raphael <raphael.lukas.m@gmail.com>
2024-04-16 15:59:49 -0300

    feat: add shadcn

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
+import { SignIn } from "./screens/signin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>
 );
